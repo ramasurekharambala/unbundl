0d48157b7fbd75a6dba11a53234b9dfd603373b9
rama surekha <ramasurekha000@gmail.com>
2025-06-30 11:29:32 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Example: Mobile Menu Toggle
+const menuToggle = document.querySelector('.menu-toggle');
+const navMenu = document.querySelector('.nav-menu');
+
+menuToggle.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+});
